af29ddfc77396f64927151d136323764b2d00a3f
Alvaro Marenco <alvaromarencom@gmail.com>
2025-06-24 12:54:07 -0600

    ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import Planets from './pages/Planets';
 import { getPlanets } from './actions/get-planets.action';
 import { Suspense } from 'react';
+import { ErrorBoundary } from './shared/ErrorBoundary';
 
 function App() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Planetas del Sistema Solar</h1>
 
-      <Suspense fallback= { <div>Cargando planetas... </div> } >
-        <Planets getPlanets= {getPlanets()} />
+      <ErrorBoundary fallback= {<div>Error al cargar los planetas...</div>} >
+        
+        <Suspense fallback= { <div>Cargando planetas... </div> } >
+          <Planets getPlanets= {getPlanets()} />
 
-      </Suspense>
+        </Suspense>
+
+      </ErrorBoundary>
+
     </div>
   );
 }
